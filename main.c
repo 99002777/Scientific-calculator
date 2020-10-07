38459e4ff2d664909e594eb457dab94dd134d23c
99002777 <72496952+99002777@users.noreply.github.com>
2020-10-08 00:00:01 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,50 @@
+#include<function.h>
+
+int main()
+{
+    int X=1;
+    char Calc_oprn;
+
+
+    // Function call
+    //calculator_operations();
+
+
+        printf("\n");
+        //printf("%s : ", KEY);
+        printf("enter the operation: ");
+
+
+        Calc_oprn=getche();
+
+        switch(Calc_oprn)
+        {
+            case '+': addition();
+                      break;
+
+            case '-': subtraction();
+                      break;
+
+            case '*': multiplication();
+                      break;
+
+            case '/': division();
+                      break;
+
+            case '?': modulus();
+                      break;
+
+            case '!': factorial();
+                      break;
+
+
+            default : system("cls");
+
+    printf("\n**********You have entered unavailable option");
+   
+
+        }
+    
+}
+
 
